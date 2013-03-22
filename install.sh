cdcb6f9bae03a983090c2eabd16daf8ae7d85751
Tony Grosinger <tony@grosinger.net>
2013-03-21 21:30:06 -0700

    Added jq

--- install.sh
+++ install.sh
@@ -4,11 +4,10 @@
 git submodule init
 git submodule update
 
-echo "This will create symlinks and destroy any conflicting configs already in place.";
-read -p "Continue? [y/N] " choice
+DIR="$( cd "$( dirname "$0" )" && pwd )"
+OS="$(lsb_release -si)"
 
 # Helper function to remove old file and link the new one
-DIR="$( cd "$( dirname "$0" )" && pwd )"
 function linkFile() {
     if [ -f $1 ]; then
         rm $1;
@@ -25,6 +24,9 @@ function createDirectory() {
     
     mkdir $1
 }
+
+echo "This will create symlinks and destroy any conflicting configs already in place.";
+read -p "Continue? [y/N] " choice
 
 # Perform the logic
 case "$choice" in 
@@ -52,6 +54,10 @@ case "$choice" in
 
         echo "Linking autoenv..."
         linkFile .autoenv
+
+        echo "Installing jq (you might have to enter your password)..."
+        chmod +x $DIR/jq
+        sudo ln -s $DIR/jq /usr/bin/jq
         
         echo "Done! Restart your shell to see changes"
     ;;
