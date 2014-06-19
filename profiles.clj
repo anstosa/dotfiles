c09e6c1b75e64f1a9c1b047154d49adb1e1c18b5
Tony Grosinger <tony@grosinger.net>
2014-06-18 17:12:55 -0700

    Adding plugins to lein profile

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,6 @@
-{:user {:dependencies [[slamhound "RELEASE"]]
+{:user {:plugins [[jonase/eastwood "0.1.2"]
+                  [lein-ancient "0.5.5"]
+                  [cider/cider-nrepl "0.6.0"]
+                  [lein-midje "3.1.3"]]
+        :dependencies [[slamhound "RELEASE"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
